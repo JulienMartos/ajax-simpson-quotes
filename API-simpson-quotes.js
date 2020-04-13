356b7b61ebbf15d1aa62cc485ecade5cc316bba3
Julien Martos <ju_sxm@hotmail.fr>
2020-04-13 17:45:37 +0200

    add rendom quote, character and image

---
+++ API-simpson-quotes.js
@@ -0,0 +1,23 @@
+// This function loads pokemon data from the Pokemon API
+function fetchSimpsonJSON() {
+  // Feel free to download this HTML and edit it, to use another Pokemon ID
+  const url = `https://simpsons-quotes-api.herokuapp.com/quotes`;
+  axios
+    .get(url)
+    .then(function (response) {
+      return response.data[0]; // response.data instead of response.json() with fetch
+    })
+    .then(function (simpson) {
+      console.log('data decoded from JSON:', simpson);
+
+      // Build a block of HTML
+      const simpsonHtml = `
+          <p><strong>${simpson.character}</strong></p>
+          <p>${simpson.quote}</p>
+          <img src="${simpson.image}" />
+        `;
+      document.querySelector('#simpson').innerHTML = simpsonHtml;
+    });
+}
+
+fetchSimpsonJSON();
